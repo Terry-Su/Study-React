94c9271d3fc66040740f2ac32690f6a179da1da4
Terry Su <2684759855@qq.com>
2017-03-06 15:41:43 +0800

    Add react-router

--- app/app.js
+++ app/app.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// apiComponent
 import Children from './apiComponent/Children'
 import Ref from './apiComponent/Ref'
 import ShouldComponentUpdate from './apiComponent/ShouldComponentUpdate'
 import ComponentWillReceiveProps from './apiComponent/ComponentWillReceiveProps'
 
+// react-router
+import BasicExample from './react-router/index'
 
 ReactDOM.render(
   <div>
+    {/* apiComponent{ */}
     {false && <Children>it's children</Children>}
     {false && <Ref />}
     {false && <ShouldComponentUpdate />}
-    {true && <ComponentWillReceiveProps />}
+    {false && <ComponentWillReceiveProps />}
+    {/* apiComponent} */}
+
+    {/* react-router{ */}
+    {true && <BasicExample />}
+    {/* react-router} */}
   </div>,
   document.getElementById('app')
 );
